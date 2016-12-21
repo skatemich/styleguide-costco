b6c0687d8f0f9352d9b4227786e8bf959e0922b7
wkaspryk <wkaspryk@pnimedia.com>
2016-12-21 14:00:15 -0800

    edits to main nav

--- public/js/script.js
+++ public/js/script.js
@@ -82,7 +82,7 @@ var transition= document.getElementById('blind');
         setTimeout(function() {
             transition.classList.add('close');
             document.body.style.overflowY= "visible";
-        }, .01);
+        }, .5);
 
 
 
