c21f80353c04c262ccd43f9d45cda1af2ee32110
wkaspryk <wkaspryk@pnimedia.com>
2017-01-06 15:35:17 -0800

    changing h3 styles

--- public/js/script.js
+++ public/js/script.js
@@ -6,5 +6,24 @@ var transition= document.getElementById('blind');
             document.body.style.overflowY= "visible";
         }, .5);
 
+// Product Thumbs
+$('.product-thumbs .next').click(function() {
+    $('.product-thumbs ul li.current').next().addClass('current');
+    $('.product-thumbs ul li.current').prev().removeClass('current');
+});
 
+$('.product-thumbs .prev').click(function() {
+    $('.product-thumbs ul li.current').prev().addClass('current');
+    $('.product-thumbs ul li.current').next().removeClass('current');
+});
 
+// Hero Slider
+$('.slider .right-arrow').click(function() {
+    $('.slider ul li.selected').next().addClass('selected');
+    $('.slider ul li.selected').prev().removeClass('selected');
+});
+
+$('.slider .left-arrow').click(function() {
+    $('.slider ul li.selected').prev().addClass('selected');
+    $('.slider ul li.selected').next().removeClass('selected');
+});
